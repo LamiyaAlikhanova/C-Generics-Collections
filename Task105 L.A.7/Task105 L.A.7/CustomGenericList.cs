759ef9766f20976ab4fe6277242c5d78f9d3e38b
LamiyaAlikhanova <you@example.com>
2024-04-18 11:28:09 +0400

    C# Generics Collections

---
+++ Task105 L.A.7/Task105 L.A.7/CustomGenericList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task105_L.A._7
+{
+    internal class CustomGenericList<T>
+    {
+        T[] array; 
+        public CustomGenericList() 
+        {
+            array=Array.Empty<T>();
+
+        }
+        public void Add(T item)
+        {
+            Array.Resize(ref array, array.Length+1);
+            array[^1] = item;
+        }
+        public T? Find (Predicate<T> match)
+        {
+            foreach (var item in array) 
+            {
+                if (match(item)) 
+                {
+                    return item;
+                }
+            }
+            return default(T);
+        } 
+        public List<T> FindAll(Predicate<T>match)
+        {
+            List<T> resultlist = new List<T>();
+            for (int i=0; i<array.Length; i++)
+            {
+                if (match(array[i]))
+                {
+                    resultlist.Add(array[i]);
+                }
+                 
+            }
+            return resultlist;
+        }
+        public bool Remove (T item)
+        {
+            for (int i=0, i<array.Length; i++)
+            {
+                if (array[i].Equals(array))
+                {
+
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ Task105 L.A.7/Task105 L.A.7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task105_L.A._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
